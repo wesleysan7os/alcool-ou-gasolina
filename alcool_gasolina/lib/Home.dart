1dec89204a13ad644a441f1090f244a73750ab2c
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-03-15 15:27:11 -0300

    criando a interface do app

--- alcool_gasolina/lib/Home.dart
+++ alcool_gasolina/lib/Home.dart
@@ -8,6 +8,62 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Álcool ou Gasolina"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              child: Text("Saiba qual a melhor opção para abastecimento do seu carro",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+                textAlign: TextAlign.left,
+              )
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Preço Alcool, ex. 1.59",
+                labelStyle: TextStyle(
+                  fontSize: 20
+                )
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço Gasolina, ex. 3.15",
+                  labelStyle: TextStyle(
+                    fontSize: 20
+                  )
+                ),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 20, left: 125, right: 125),
+                child: Text("Calcular",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
